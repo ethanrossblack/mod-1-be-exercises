65f7a1277608953991fd63a149c33c1c3345fab0
Ethan Black <ethanrossblack@gmail.com>
2023-05-17 12:35:11 -0600

    test: add initialization tests

--- practice_independent_challenges/week2/potluck/spec/dish_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -2,13 +2,33 @@ require './lib/dish'
 require "RSpec"
 
 describe Dish do
-  before
-  
-  context "#initialize" do
-    it "exists" do
-
-    end
+  before(:each) do
+    @dish = Dish.new("Couscous Salad", :appetizer)
   end
 
+  context "#initialize" do
+    it "exists" do
+      expect(@dish).to be_a(Dish)
+    end
 
+    it "has a name" do
+      expect(@dish.name).to eq("Couscous Salad")
+    end
+
+    it "has a category" do
+      expect(@dish.category).to eq(:appetizer)
+    end
+
+    it "needs a name" do
+      expect{ 
+        nameless_dish = Dish.new(:appteizer)
+      }.to raise_error
+    end
+
+    it "needs a category" do
+      expect{ 
+        uncategorized_dish = Dish.new(:appteizer)
+      }.to raise_error
+    end
+  end
 end
